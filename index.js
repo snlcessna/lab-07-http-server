3f07285031edc514b4f8473ff84ba7496fda893e
David Boucher <david.boucher@ookla.com>
2017-10-13 12:38:15 -0700

    Finish testing and merge with master

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const server = require('./lib.server.js');
+const server = require('./lib/server.js');
 
 require('dotenv').config();
 
-server.start(process.env.PORT | 5000);
+server.listen(process.env.PORT || 5000);
